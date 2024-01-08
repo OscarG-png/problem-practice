5428c85f67e0e33aa04e448be414c6874e458dda
OscarG <oscar.gamez88@gmail.com>
2024-01-08 14:13:31 -0800

    fith solution

---
+++ codewars/python solutions/complement-dna.py
@@ -0,0 +1,16 @@
+def DNA_strand(dna):
+    result = ""
+
+    for char in dna:
+        if char == "A":
+            result += "T"
+        elif char == "T":
+            result += "A"
+        elif char == "C":
+            result += "G"
+        elif char == "G":
+            result += "C"
+    return result
+
+
+# not the prettiest solution, but it works
