04b8b13fd49213317c23bf5f45f3d080999f2266
OscarG <oscar.gamez88@gmail.com>
2024-01-08 14:10:39 -0800

    fourth solution

---
+++ codewars/python solutions/expression-matters.py
@@ -0,0 +1,12 @@
+def expression_matter(a, b, c):
+    results = [
+        a + b + c,
+        a * b * c,
+        a * (b + c),
+        (a + b) * c,
+        a + b * c,
+        a * b + c,
+        (a + b) * (c),
+        a * (b * c),
+    ]
+    return max(results)
