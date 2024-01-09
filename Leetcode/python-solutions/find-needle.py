9e343b9ef47fd216b7d7fd615b1df572d9c77df5
OscarG <oscar.gamez88@gmail.com>
2024-01-09 10:49:27 -0800

    updated solution 4

--- Leetcode/python-solutions/find-needle.py
+++ Leetcode/python-solutions/find-needle.py
@@ -5,7 +5,16 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        return haystack.find(needle)
+        # first solution
+        # I didn't think this solution would work but
+        # the runtime is pretty slow
+        # at least the memory usage is middle of the road.
+        # return haystack.find(needle)
 
-# I didn't think this solution would work but the runtime is pretty slow
-# at least the memory usage is middle of the road.
+        # second solution
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1
+
+# This solution is a lot faster on runtime but uses more memory.
