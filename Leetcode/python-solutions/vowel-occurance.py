d7b94bf0c2f700f7c9ff7a6406a97c24d5c18bb3
OscarG <oscar.gamez88@gmail.com>
2024-01-17 12:27:32 -0800

    second solution

---
+++ Leetcode/python-solutions/vowel-occurance.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxVowels(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        vowels = set('aeiou')
+        current_vowels = sum(1 for char in s[:k] if char in vowels)
+        max_vowels = current_vowels
+
+        for i in range(k, len(s)):
+            if s[i] in vowels:
+                current_vowels += 1
+            if s[i - k] in vowels:
+                current_vowels -= 1
+            max_vowels = max(max_vowels, current_vowels)
+
+        return max_vowels
