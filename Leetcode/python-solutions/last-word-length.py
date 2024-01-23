a720389d86a454979b5a76349b036614a580b274
OscarG <oscar.gamez88@gmail.com>
2024-01-23 13:36:58 -0800

    refactor

--- Leetcode/python-solutions/last-word-length.py
+++ Leetcode/python-solutions/last-word-length.py
@@ -15,3 +15,11 @@ class Solution(object):
 
 
 # surprised this did really good on runtime and memory.
+# this variation is 8ms slower but saves like .12mb of memory
+        # words = [
+        #     word for word in s.split(' ') if word
+        # ]
+        # if not words:
+        #     return 0
+
+        # return len(words[-1])
