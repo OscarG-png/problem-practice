8c30181f714b04ce590d3b72e877a73aec88813f
OscarG <oscar.gamez88@gmail.com>
2024-01-30 12:03:36 -0800

    first solution

---
+++ Leetcode/python-solutions/num-palindron.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        numStr = str(x)
+        return numStr == numStr[::-1]
