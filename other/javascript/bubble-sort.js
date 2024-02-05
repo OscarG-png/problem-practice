ca06dd9ea5f9941b380f4dfb5bf437e5f045cd94
OscarG <oscar.gamez88@gmail.com>
2024-02-05 14:47:47 -0800

    bubble sort in js

---
+++ other/javascript/bubble-sort.js
@@ -0,0 +1,12 @@
+function bubbleSort(items) {
+    const length = items.length;
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - i - 1; j++) {
+            if (items[j] > items[j + 1]) {
+                const temp = items[j];
+                items[j] = items[j + 1];
+                items[j + 1] = temp;
+            }
+        }
+    }
+}
