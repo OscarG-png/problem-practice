89f79a2e3b5971215f80f9d6410dda2415561885
OscarG <oscar.gamez88@gmail.com>
2024-02-05 10:33:15 -0800

    alernate solution for previous problem

--- Leetcode/python-solutions/top-k-frequency.py
+++ Leetcode/python-solutions/top-k-frequency.py
@@ -14,3 +14,18 @@ class Solution(object):
         sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
         result = [num for num, _ in sorted_freq[:k]]
         return result
+
+# alternate solution
+        # count = {}
+        # freq = [[] for i in range(len(nums) + 1)]
+
+        # for num in nums:
+        #     count[num] = 1 + count.get(num, 0)
+        # for n, c in count.items():
+        #     freq[c].append(n)
+        # result = []
+        # for i in range(len(freq) -1, 0, -1):
+        #     for n in freq[i]:
+        #         result.append(n)
+        #         if len(result) == k:
+        #             return result
