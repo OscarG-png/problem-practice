2250fb4b4c5fdeeac43b6160d569be87655235a2
OscarG <oscar.gamez88@gmail.com>
2024-02-17 14:54:30 -0800

    another one

---
+++ Leetcode/python-solutions/move-zeros.py
@@ -0,0 +1,7 @@
+class soltution:
+    def moveZeros(self, nums):
+        l = 0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
