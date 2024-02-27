194b505b0e26fd8c58550b7a0e9cd29701b9d0dd
OscarG <oscar.gamez88@gmail.com>
2024-02-27 11:52:14 -0800

    optimized solution

--- Leetcode/python-solutions/meeting-rooms.py
+++ Leetcode/python-solutions/meeting-rooms.py
@@ -1,7 +1,9 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort()
+        intervals.sort(key = lambda x: x[0])
         for i in range(len(intervals) - 1):
             if intervals[i][1] > intervals[i + 1][0]:
                 return False
         return True
+
+# changing sort from just intervals.sort() to intervals.sort(key = lambda x: x[0]) decreased my runtime from 83 to 72 ms
