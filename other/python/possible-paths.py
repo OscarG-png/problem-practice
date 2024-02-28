9d79bdae86289bf08f768beebd259e4c6949db2a
OscarG <oscar.gamez88@gmail.com>
2024-02-28 14:28:25 -0800

    something i worked on

---
+++ other/python/possible-paths.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+def possible_paths(grid, robotLocation, maxSteps):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    def validMove(row, col, steps):
+        return 0 <= row < rows and 0 <= col < cols and grid[row][col] != "X"
+
+    def dfs(row, col, steps):
+        if steps == 0:
+            return 0
+        if grid[row][col] == "C":
+            return 1
+        count = 0
+        grid[row][col] = "X"
+        for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            new_row = row + dr
+            new_col = col + dc
+            if validMove(new_row, new_col):
+                count += dfs(new_row, new_col, steps - 1)
+        grid[row][col] = "0"
+        return count
+    start_row, start_col = robotLocation
+    return dfs(start_row, start_col, maxSteps)
+
+
+class TestPossiblePaths(unittest.TestCase):
+    def test_possible_paths(self):
+        grid = [
+            ['0', '0', '0', '0', '0', '0', 'X', 'C'],
+            ['0', '0', '0', '0', 'C', '0', 'X', 'X'],
+            ['0', '0', 'S', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', 'C', 'X']
+        ]
+        robotLocation = [2, 2]
+        maxSteps = 3
+        result = possible_paths(grid, robotLocation, maxSteps)
+        # Replace 'expected_result' with the expected output of the function
+        expected_result = 3
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
