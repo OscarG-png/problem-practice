84e4fc2a4e279b7bda0599f60f6f567e1fed1b25
OscarG <oscar.gamez88@gmail.com>
2024-03-26 08:18:30 -0700

    catching up

---
+++ Leetcode/python-solutions/find-dups-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+        n = len(nums)
+
+        for x in nums:
+            x = abs(x)
+            if nums[x - 1] < 0:
+                res.append(x)
+            nums[x - 1] *= -1
+        return res
