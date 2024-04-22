367fd81479e51284a1f26a7f9c692740ada0c802
OscarG <oscar.gamez88@gmail.com>
2024-04-22 09:13:51 -0700

    redid old solution

--- Leetcode/typescript/contains-duplicate.ts
+++ Leetcode/typescript/contains-duplicate.ts
@@ -1,11 +1,12 @@
 function containsDuplicate(nums: number[]): boolean {
-  let map = {};
+  let count: Set<number> = new Set();
 
   for (let num of nums) {
-    if (map[num]) {
+    if (count.has(num)) {
       return true;
+    } else {
+      count.add(num);
     }
-    map[num] = 1;
   }
   return false;
 }
