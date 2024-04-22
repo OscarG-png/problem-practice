e1126496eac06b101a33bc6826d6b76006a52431
OscarG <oscar.gamez88@gmail.com>
2024-04-22 09:29:01 -0700

    asserted typing on previous solution

--- Leetcode/typescript/two-sum.ts
+++ Leetcode/typescript/two-sum.ts
@@ -1,11 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
-  let numMap = new Map();
+  let numMap: Map<number, number> = new Map();
 
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
 
     if (numMap.has(complement)) {
-      return [numMap.get(complement), i];
+      return [numMap.get(complement)!, i];
     }
     numMap.set(nums[i], i);
   }
