48a44da6bac8a29f49ea8b7701e4527a39b15fb8
OscarG <oscar.gamez88@gmail.com>
2024-05-22 13:26:07 -0700

    another old solution in typescript

---
+++ Leetcode/typescript/reverse-linked-list.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return null;
+  }
+  let prev: ListNode = null;
+
+  while (head) {
+    const next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev;
+}
