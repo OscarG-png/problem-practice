a53e6109bbd8b865ceea1e8996118168dcaa93ac
OscarG <oscar.gamez88@gmail.com>
2024-06-26 20:01:08 -0700

    fixed issues

--- Leetcode/python-solutions/coin-change.py
+++ Leetcode/python-solutions/coin-change.py
@@ -1,5 +1,6 @@
+from typing import List
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
+    def coinChange(self, coins: List[int], amount: int) -> int | float:
         dp = [0] + ([float('inf')] * amount)
 
         for i in range(1, amount + 1):
