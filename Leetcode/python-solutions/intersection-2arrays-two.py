8f267c6e151cd786ca554d41c941d981bf55840d
OscarG <oscar.gamez88@gmail.com>
2024-07-02 12:56:40 -0700

    decided to add a version that didn't use a build in method

--- Leetcode/python-solutions/intersection-2arrays-two.py
+++ Leetcode/python-solutions/intersection-2arrays-two.py
@@ -7,6 +7,11 @@ class Solution:
 
         count = Counter(nums1)
 
+        # incase you want to see it done by hand
+        # count = {}
+        # for num in nums1:
+            # count[num] = count.get(num, 0) + 1
+
         for num in nums2:
             if count[num] > 0:
                 res.append(num)
