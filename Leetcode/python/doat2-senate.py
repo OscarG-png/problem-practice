f9e34132b5b1a5aea3eeda01e2daf71bfea94559
OscarG <oscar.gamez88@gmail.com>
2024-07-16 11:12:18 -0700

    another solution for the day

---
+++ Leetcode/python/doat2-senate.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        senate = list(senate)
+        n = len(senate)
+
+        rStack, dStack = deque(), deque()
+        for i, c in enumerate(senate):
+            if c == "R":
+                rStack.append(i)
+            else:
+                dStack.append(i)
+        while rStack and dStack:
+            currR = rStack.popleft()
+            currD = dStack.popleft()
+
+            if currR < currD:
+                rStack.append(currD + n)
+            else:
+                dStack.append(currR + n)
+
+        return "Radiant" if rStack else "Dire"
