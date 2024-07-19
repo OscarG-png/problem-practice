355e1c610cf1fc6667f6a738bdca069378db71d6
OscarG <oscar.gamez88@gmail.com>
2024-07-19 10:53:27 -0700

    better solution for lc daily

--- Leetcode/python/lucky-number-matrix.py
+++ Leetcode/python/lucky-number-matrix.py
@@ -3,20 +3,18 @@ from typing import List
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
         ROWS, COLS = len(matrix), len(matrix[0])
-        res = []
 
-        row_min = set()
+        max_of_row_mins = float('-inf')
         for r in range(ROWS):
-            row_min.add(min(matrix[r]))
-        col_max = set()
+            row_mins = min(matrix[r])
+            max_of_row_mins = max(row_mins, max_of_row_mins)
+
         for c in range(COLS):
-            cur_max = matrix[0][c]
+            col_max = matrix[0][c]
+
             for r in range(ROWS):
-                cur_max = max(cur_max, matrix[r][c])
-            col_max.add(cur_max)
+                col_max = max(col_max, matrix[r][c])
+            if col_max == max_of_row_mins:
+                return [col_max]
 
-        for n in row_min:
-            if n in col_max:
-                res.append(n)
-
-        return res
+        return []
