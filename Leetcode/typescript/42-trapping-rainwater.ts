a0ab42d460bc424213079b3c6d9329851dcfd20c
OscarG <oscar.gamez88@gmail.com>
2024-07-30 11:14:11 -0700

    more ts practice

---
+++ Leetcode/typescript/42-trapping-rainwater.ts
@@ -0,0 +1,20 @@
+function trap(height: number[]): number {
+    let l = 0;
+    let r = height.length - 1;
+    let leftMax = height[l];
+    let rightMax = height[r];
+    let res = 0;
+
+    while (l < r) {
+        if (leftMax < rightMax) {
+            l++;
+            leftMax = Math.max(leftMax, height[l]);
+            res += leftMax - height[l];
+        } else {
+            r--;
+            rightMax = Math.max(rightMax, height[r]);
+            res += rightMax - height[r];
+        }
+    }
+    return res;
+}
