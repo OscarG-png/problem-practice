2ed94a804771ca7b1555cd82bc8f70e205f73061
OscarG <oscar.gamez88@gmail.com>
2024-08-01 10:13:08 -0700

    updated old solution

--- Leetcode/python/move-zeros.py
+++
@@ -1,7 +0,0 @@
-class soltution:
-    def moveZeros(self, nums):
-        l = 0
-        for r in range(len(nums)):
-            if nums[r]:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
