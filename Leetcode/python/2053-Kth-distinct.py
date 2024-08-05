0a486a0d99110fe3fedc6ed60c65814b220ec6f4
OscarG <oscar.gamez88@gmail.com>
2024-08-05 09:51:34 -0700

    leetcode daily 08/05/24

---
+++ Leetcode/python/2053-Kth-distinct.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        count = {}
+
+        for s in arr:
+            count[s] = count.get(s, 0) + 1
+
+        for s in arr:
+            if count[s] == 1:
+                k -= 1
+                if k == 0:
+                    return s
+        return ''
