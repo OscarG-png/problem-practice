2fec5148c93aac7373dc0ba474109ede5e691be7
OscarG <oscar.gamez88@gmail.com>
2024-10-10 10:23:01 -0700

    Leetcode daily 10/10/24

---
+++ Leetcode/python/962-max-width-ramp.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        maxRight = [0] * len(nums)
+        i = len(nums) - 1
+        prevMax = 0
+
+        for n in reversed(nums):
+            maxRight[i] = max(n, prevMax)
+            prevMax = maxRight[i]
+            i -= 1
+
+        res = 0
+        l = 0
+        for r in range(len(nums)):
+            while nums[l] > maxRight[r]:
+                l += 1
+            res = max(res, r - l)
+        return res
