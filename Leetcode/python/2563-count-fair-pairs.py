b04df2aabfc6202e19917b724fc8cb9b7003f6a7
OscarG <oscar.gamez88@gmail.com>
2024-11-13 10:12:23 -0800

    leetcode daily 11/13/2024

---
+++ Leetcode/python/2563-count-fair-pairs.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        def binSearch(l, r, target):
+            while l <= r:
+                m = (l + r) // 2
+                if nums[m] >= target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            return r
+
+        nums.sort()
+        res = 0
+
+        for i in range(len(nums)):
+            low = lower - nums[i]
+            up = upper - nums[i]
+
+            res += (
+                binSearch(i + 1, len(nums) - 1, up + 1) -
+                binSearch(i + 1, len(nums) - 1, low)
+            )
+        return res
