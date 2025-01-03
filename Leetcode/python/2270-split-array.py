3d2f206459792c26bc86b8171624ed2be30686cb
OscarG <oscar.gamez88@gmail.com>
2025-01-03 13:35:45 -0800

    leetcode daily 1/3/25

---
+++ Leetcode/python/2270-split-array.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        res = 0
+        right = sum(nums)
+        left = 0
+
+        for i in range(len(nums) - 1):
+            left += nums[i]
+            right -= nums[i]
+
+            res += 1 if left >= right else 0
+
+        return res
