d67680234534eda6dfe92d7df01cf0907a97fe65
OscarG <oscar.gamez88@gmail.com>
2025-01-11 09:51:23 -0800

    leetcode daily 1/11/25

---
+++ Leetcode/python/1400-k-palindrome-strings.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if k > len(s):
+            return False
+
+        count = Counter(s)
+        odd = 0
+
+        for cnt in count.values():
+            odd += cnt % 2
+
+        return odd <= k
